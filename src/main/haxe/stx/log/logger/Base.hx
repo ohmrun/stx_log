036d23696125a2cbc9e6869a210d2cdc712584ea
0b1kn00b <polysemantic@gmail.com>
2021-07-13 12:08:46 +0200

    stx.logl.logger.Base not checked in

---
+++ src/main/haxe/stx/log/logger/Base.hx
@@ -0,0 +1,45 @@
+package stx.log.logger;
+
+class Base<T> implements LoggerApi<T> extends stx.log.output.term.Full{
+  
+  public function new(?logic:Filter<T>,?format:Format){
+    this.logic  = __.option(logic).def(Filter.Unit);
+    this.format = __.option(format).defv(Format.unit());
+  }
+
+  public var logic  : stx.log.Logic<T>;
+  public var format : Format;
+
+  final public function apply(value:Value<T>):Continuation<Res<String,LogFailure>,Value<T>>{
+    return do_apply(value).mod(
+      (res) -> {
+        return res.map(
+          __.passthrough(
+            (string:String) -> { 
+              //trace('about to render: ${value.stamp}');
+              if(!value.stamp.hidden){
+                #if macro
+                  render(string,value.source);
+                  // if(value.stamp.level != CRAZY){
+                   
+                  // }
+                #else
+                  render(string,value.source);
+                #end
+              }
+            }
+          )
+        );
+      }
+    );
+  }
+
+  private function do_apply(value:Value<T>):Continuation<Res<String,LogFailure>,Value<T>>{
+    return Continuation.lift(
+      (fn:Value<T>->Res<String,LogFailure>) -> {
+        var result = logic.apply(value).resolve(() -> this.format.print(value));
+        return result;
+      }
+    );
+  }
+} 
