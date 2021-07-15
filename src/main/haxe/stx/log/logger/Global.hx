9568d100a3f180154e528000845f7099dea15890
0b1kn00b <polysemantic@gmail.com>
2021-07-15 08:30:07 +0200

    global ballsup

--- src/main/haxe/stx/log/logger/Global.hx
+++ src/main/haxe/stx/log/logger/Global.hx
@@ -1,13 +1,12 @@
 package stx.log.logger;
 
 @:forward abstract Global(stx.log.logger.Unit) to stx.log.logger.Unit from stx.log.logger.Unit{
-  static public var ZERO(default,never) = unit();
-  @:isVar public static var instance(get,null):stx.log.logger.Unit;
-  static private function get_instance(){
-    return instance == null ? instance = new stx.log.logger.Unit() : instance;
+  static public var ZERO(get,never) = unit();
+  static private function get_ZERO(){
+    return ZERO == null ? ZERO = new stx.log.logger.Unit() : ZERO;
   }
   public function new(){
-    this = instance;
+    this = ZERO;
   }
   static public function unit(){
     return new Global();
