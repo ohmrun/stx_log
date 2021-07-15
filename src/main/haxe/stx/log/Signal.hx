b367f7010c8e9eea572882b1f6028e3f8bb44395
0b1kn00b <polysemantic@gmail.com>
2021-07-15 08:34:20 +0200

    global ballsup#2

--- src/main/haxe/stx/log/Signal.hx
+++ src/main/haxe/stx/log/Signal.hx
@@ -39,7 +39,7 @@ typedef SignalDef = SignalCls;
       instance.attach(new DebugLogger());
       has_custom = false;
     #end
-    var facade = Logger.ZERO;
+    var facade = Global.ZERO;
     instance.attach(facade);
     has_custom = false;
   }
--- src/main/haxe/stx/log/logger/Global.hx
+++ src/main/haxe/stx/log/logger/Global.hx
@@ -1,7 +1,7 @@
 package stx.log.logger;
 
 @:forward abstract Global(stx.log.logger.Unit) to stx.log.logger.Unit from stx.log.logger.Unit{
-  static public var ZERO(get,never) = unit();
+  static public var ZERO(get,null);
   static private function get_ZERO(){
     return ZERO == null ? ZERO = new stx.log.logger.Unit() : ZERO;
   }
